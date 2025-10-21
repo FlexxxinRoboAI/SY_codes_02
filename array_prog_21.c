b7ce3136b8ebd8a2c3b5e3296393a01ada28f616
Ganaaa <ganeshpawar9239@gmail.com>
2025-10-21 22:45:09 +0530

    Array_prog

---
+++ array_prog_21.c
@@ -0,0 +1,35 @@
+// find the smallest prime number in an array
+#include <stdio.h>
+
+int isPrime(int num) {
+    if(num < 2) return 0;
+    for(int i = 2; i*i <= num; i++) {
+        if(num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n, i, minPrime = -1;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if(isPrime(arr[i])) {
+            if(minPrime == -1 || arr[i] < minPrime)
+                minPrime = arr[i];
+        }
+    }
+
+    if(minPrime == -1)
+        printf("No prime numbers found in the array.\n");
+    else
+        printf("Smallest prime element = %d\n", minPrime);
+
+    return 0;
+}
