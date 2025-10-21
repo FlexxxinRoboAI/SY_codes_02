2221cd4b588857ae60f40003e7371161ebc1ef65
Ganaaa <ganeshpawar9239@gmail.com>
2025-10-21 22:09:43 +0530

    Array_prog

---
+++ array_prog_07.c
@@ -0,0 +1,24 @@
+// count the number of even and odd elements in an array
+#include <stdio.h>
+
+int main() {
+    int n, i, evenCount = 0, oddCount = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if(arr[i] % 2 == 0)
+            evenCount++; // count even numbers
+        else
+            oddCount++;  // count odd numbers
+    }
+
+    printf("Count of even elements = %d\n", evenCount);
+    printf("Count of odd elements = %d\n", oddCount);
+
+    return 0;
+}
