f162ce629ab9c91605a33ef0d0a1aca8c2d3a4a2
Ganaaa <ganeshpawar9239@gmail.com>
2025-10-24 10:36:21 +0530

    Array_prog

---
+++ array_prog_42.c
@@ -0,0 +1,54 @@
+// multiply two matrices
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2, i, j, k;
+
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    // Matrix multiplication rule
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible! Columns of first must equal rows of second.\n");
+        return 0;
+    }
+
+    int mat1[r1][c1], mat2[r2][c2], result[r1][c2];
+
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < r1; i++)
+        for (j = 0; j < c1; j++)
+            scanf("%d", &mat1[i][j]);
+
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < r2; i++)
+        for (j = 0; j < c2; j++)
+            scanf("%d", &mat2[i][j]);
+
+    // Initialize result matrix with 0
+    for (i = 0; i < r1; i++)
+        for (j = 0; j < c2; j++)
+            result[i][j] = 0;
+
+    // Multiplication logic
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c2; j++) {
+            for (k = 0; k < c1; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+
+    printf("\nResultant Matrix (Multiplication):\n");
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
