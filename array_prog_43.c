7c5d90784ee9da6c9b58089f5f4dd10daf58f3a8
Ganaaa <ganeshpawar9239@gmail.com>
2025-10-24 10:37:27 +0530

    Array_prog

---
+++ array_prog_43.c
@@ -0,0 +1,37 @@
+// find the transpose of a matrix
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    int mat[r][c], trans[c][r];
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    // Transpose logic: swap rows and columns
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            trans[j][i] = mat[i][j];
+        }
+    }
+
+    printf("\nTranspose of the matrix:\n");
+    for (i = 0; i < c; i++) {
+        for (j = 0; j < r; j++) {
+            printf("%d ", trans[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
